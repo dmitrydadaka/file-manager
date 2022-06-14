22b277ec8b31322d5186e706b60684433f3ddb69
User <belpc2012@gmail.com>
2022-06-14 17:56:25 +0300

    feat: os part nodejs in project

--- utils.mjs
+++ utils.mjs
@@ -1,5 +1,5 @@
-import path from 'path';
-import fs from 'fs';
+import os from 'os';
+
 import { rl } from './app.mjs';
 import { read } from './read.mjs';
 import { create } from './create.mjs';
@@ -34,40 +34,64 @@ export const switcher = async (uInput) => {
                 break;
             case 'cat':
                 try {
-                await read(secondArg);
+                    await read(secondArg);
                 } catch (e) {
                     console.log('Operation failed');
                 }
                 break;
             case 'add':
                 try {
-                await create(process.cwd(), secondArg);
+                    await create(process.cwd(), secondArg);
                 } catch (e) {
                     console.log('Operation failed');
                 }
                 break;
-            case 'rn': 
-            await rn(secondArg, thirdArg);
-            break;
-            case 'rm': 
-            await rm(secondArg);
-            break;
+            case 'rn':
+                await rn(secondArg, thirdArg);
+                break;
+            case 'rm':
+                await rm(secondArg);
+                break;
             case 'cp':
-            await cp(secondArg, thirdArg);
-            break;
-            case 'mv': 
-            await cp(secondArg, thirdArg);
-            await rm(secondArg);
-            break;
+                await cp(secondArg, thirdArg);
+                break;
+            case 'mv':
+                await cp(secondArg, thirdArg);
+                await rm(secondArg);
+                break;
             case 'compress':
-            await compress(secondArg, thirdArg);
-            break;
+                await compress(secondArg, thirdArg);
+                break;
             case 'decompress':
-            await decompress(secondArg, thirdArg);
-            break;
-            case 'hash': 
-            await hash(secondArg);
-            break;
+                await decompress(secondArg, thirdArg);
+                break;
+            case 'hash':
+                await hash(secondArg);
+                break;
+            case 'os':
+                switch (secondArg) {
+                    case '--EOL':
+                        process.stdout.write(JSON.stringify(os.EOL) + '\n');
+                        break;
+                    case '--cpus':
+                        console.log('Overall amount of CPUS: ', os.cpus().length);
+                        for (const el of os.cpus()) {
+                            console.log('model and clock rate (in GHz): ', el.model);
+                        }
+                        break;
+                    case '--homedir':
+                        console.log(os.homedir())
+                        break;
+                    case '--username':
+                        console.log(os.userInfo().uid);
+                        break;
+                    case '--architecture':
+                        console.log(process.arch)
+                        break;
+                    default:
+                        console.log('Invalid input! Please try again.\n');
+                }
+                break;
             default:
                 rl.setPrompt('Invalid input! Please try again.\n');
                 rl.prompt();
